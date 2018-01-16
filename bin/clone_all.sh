7995d097e95d079aca4a8f2f55417e31a23b8e51
Steven Soloff <steven.soloff@gmail.com>
2018-01-15 23:32:47 -0500

    Use writable URL when cloning map repos
    The Git protocol does not support authentication, and thus cannot be used
    to push changes back to GitHub.  The clone_all.sh script was modified to
    clone all repos using the SSH protocol, which does permit changes to be
    pushed back to GitHub (assuming the user has registered their SSH key
    with GitHub).

--- bin/clone_all.sh
+++ bin/clone_all.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-
- ## note:
- ##  -  it appears that it takes some time for a repo to show up in an orgs repo list
- ##  -  the curl call below is rate limited, not intended for repeated invocations
+## note:
+##  -  it appears that it takes some time for a repo to show up in an orgs repo list
+##  -  the curl call below is rate limited, not intended for repeated invocations
 
 for j in 1 2; do
-  while read gitUrl; do 
-    echo "clone $gitUrl"; 
-    git clone $gitUrl; 
-  done < <(curl --silent "https://api.github.com/orgs/triplea-maps/repos?page=$j&per_page=1000" | grep git_url  | sed 's/.*git:/git:/' | sed 's/",//')
+  while read gitUrl; do
+    echo "clone $gitUrl"
+    git clone $gitUrl
+  done < <(curl --silent "https://api.github.com/orgs/triplea-maps/repos?page=$j&per_page=1000" | grep ssh_url  | sed 's/\s*"ssh_url": "//' | sed 's/",//')
 done
-
-
-
